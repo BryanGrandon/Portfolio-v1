6c973ac5f6cecb57e8cd7d7076ca98b5f9612a89
Bryan Grandon <bryan.grandon.c@gmail.com>
2025-03-20 11:08:10 -0300

    refactor(web):add dependencies, scss folder and change structure

---
+++ src/core/App.tsx
@@ -0,0 +1,11 @@
+import { JSX } from 'react'
+
+const App = (): JSX.Element => {
+  return (
+    <>
+      <h1>Portfolio</h1>
+    </>
+  )
+}
+
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
+/// <reference types="vitest"/>
+/// <reference types="vite/client"/>
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  test: {
+    environment: 'happy-dom',
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler', // or "modern"
+      },
+    },
+  },
 })
