1987da364a581ce50a3e7ea00c5f3cf58ad79823
Bryan Grandon <bryan.grandon.c@gmail.com>
2025-03-28 14:39:44 -0300

    feat(web):add main-theme.tsx

--- src/core/components/layout/main.tsx
+++ src/core/components/layout/main.tsx
@@ -1,32 +1,31 @@
 import data from '../../services/data.json' assert { type: 'json' }
 import ProjectCard from '../ui/project-card'
-import Title from '../ui/title'
 import { AllIconsStack } from '../ui/icons'
+import MainTheme from './main-theme'
 
 const Main = () => {
   return (
     <main className='main'>
-      <article id='projects'>
-        <Title text='Projects' />
-        <article className='projects'>
-          {data.projects.map((e) => (
-            <ProjectCard key={e.id} url={e.url} img={e.img} title={e.title} description={e.description} ariaLabel={e.ariaLabel} />
-          ))}
-        </article>
-      </article>
+      <MainTheme
+        title='Projects'
+        class_A='main__projects'
+        children={
+          <>
+            {data.projects.map((e) => (
+              <ProjectCard
+                key={e.id}
+                url={e.url}
+                img={e.img}
+                title={e.title}
+                description={e.description}
+                ariaLabel={e.ariaLabel}
+              />
+            ))}
+          </>
+        }
+      />
 
-      <article id='Stacks'>
-        <Title text='Stacks' />
-        <section className='list-stack'>{AllIconsStack('list-stack__icons')}</section>
-      </article>
-
-      <article id='about'>
-        <Title text='About' />
-      </article>
-
-      <article id='contact'>
-        <Title text='Contact' />
-      </article>
+      <MainTheme title='Stacks' children={<section className='list-stack'>{AllIconsStack('list-stack__icons')}</section>} />
     </main>
   )
 }
