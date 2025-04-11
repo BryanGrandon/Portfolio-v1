c177d6000fbb228c4849bb36fb6c942abb009dcb
Bryan Grandon <bryan.grandon.c@gmail.com>
2025-04-11 17:54:22 -0400

    ready to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: 'https://BryanGrandon.github.io/Portfolio/',
   test: {
     environment: 'happy-dom',
   },
