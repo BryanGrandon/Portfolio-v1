8967962721ca3acd9d54bad11ed5352f556b3970
Bryan Grandon <bryan.grandon.c@gmail.com>
2025-04-14 14:59:07 -0400

    refactor(web):try to fix the images

--- src/core/components/ui/project-card.tsx
+++ src/core/components/ui/project-card.tsx
@@ -12,7 +12,7 @@ type Project_Card = {
 const ProjectCard = ({ image, url, title, description, ariaLabel, technologies }: Project_Card) => {
   return (
     <a href={url} target='__black' className='project-card' aria-label={ariaLabel}>
-      <img className='project-card__img' src={image.src} alt={image.alt} />
+      <img className='project-card__img' src={image.src} alt={image.alt} typeof='image/webp' />
       <section className='project-card__info'>
         <h3 className='project-card__title'>{title}</h3>
         <p style={{ position: 'relative' }}>{description}</p>
