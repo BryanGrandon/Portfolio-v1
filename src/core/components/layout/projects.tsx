3e2c64ebb8fe5bece78ee4424e08b2f75a659098
Bryan Grandon <bryan.grandon.c@gmail.com>
2025-06-23 20:05:58 -0400

    feat(web):change data.json for add midas-touch project

--- src/core/components/layout/projects.tsx
+++ src/core/components/layout/projects.tsx
@@ -5,19 +5,14 @@ import Title from '../ui/title'
 
 const Projects = (): JSX.Element => {
   const projects = data.projects
+  console.log(projects[2].url)
+
   return (
     <article className='projects'>
       <Title text='Projects' />
       <section className='projects__container'>
         {projects.map((e) => (
-          <ProjectCard
-            key={e.id}
-            url={e.url}
-            title={e.title}
-            description={e.description}
-            ariaLabel={e.ariaLabel}
-            technologies={e.technologies}
-          />
+          <ProjectCard key={e.id} url={e.url} title={e.title} description={e.description} ariaLabel={e.ariaLabel} technologies={e.technologies} />
         ))}
       </section>
     </article>
